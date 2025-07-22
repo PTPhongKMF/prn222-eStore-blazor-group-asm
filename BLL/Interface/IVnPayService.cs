0d5ad9eabf623b4235d6e21a71ebbed9eea48b0c
td7legend <td7legend@gmail.com>
2025-07-22 14:13:53 +0700

    Add VnPay setup

---
+++ BLL/Interface/IVnPayService.cs
@@ -0,0 +1,12 @@
+﻿
+using DAL.Entities;
+using Microsoft.AspNetCore.Http;
+namespace BLL.Interface
+{
+    public interface IVnPayService
+    {
+        string CreatePaymentUrl(PaymentInformationModel model, HttpContext context);
+        PaymentResponseModel PaymentExecute(IQueryCollection collections);
+    }
+
+}
